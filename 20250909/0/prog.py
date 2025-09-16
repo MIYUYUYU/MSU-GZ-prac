5cd11b3fba4313c60b33725b0fcaf6213163de1d
GZ <469565267@qq.com>
2025-09-17 03:41:59 +0800

    homechange

---
+++ 20250909/0/prog.py
@@ -0,0 +1,9 @@
+a, b = eval(input())
+if a > b:
+    print(a)
+else:
+    print(b)
+
+#enter
+import sys
+exec(sys.stdin.read())
---
+++ 20250909/1/prog.py
@@ -0,0 +1,7 @@
+a = "QQ"
+print(a)
+
+#enter
+import sys
+exec(sys.stdin.read())
+
