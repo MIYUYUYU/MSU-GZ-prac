26c35306cfacfa7e7b57521745e373cf1e7057e7
GZ <469565267@qq.com>
2025-09-17 04:48:58 +0800

    New change

---
+++ 20250909/1/wprog.py
@@ -0,0 +1,23 @@
+a = eval(input())
+
+
+##i, j = 0, 0
+##tmp = a[0]
+##while i < 3:
+##    j = i + 1
+##    while j < 3:
+##        if a[i] > a[j]:
+##            tmp = a[i]
+##            a[i] = a[j]
+##            a[j] = tmp
+##        j += 1
+##    i += 1
+
+
+print(a[0],a[1],a[2],sep=', ')
+
+
+#enter
+import sys
+exec(sys.stdin.read())
+
