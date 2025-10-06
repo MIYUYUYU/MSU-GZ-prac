66ea48d89e0f6e62cd1ea77b932f790d31b80727
GZ <GZ@MR>
2025-10-06 14:05:38 +0300

    hw4

--- 20250930/3/prog.py
+++ 20250930/3/prog.py
@@ -4,15 +4,15 @@ s, t, u = eval(input())
 #print(f"s = {s} t = {t} u = {u}")
 x_val = eval(input())
 def Calc(s, t, u):
+    def inner(x):
+        x = x_val
+        s_result = eval(s,{'x': x},globals())
+        t_result = eval(t,{'x': x},globals())
 
-    x = x_val
-    s_result = eval(s,{'x': x},globals())
-    t_result = eval(t,{'x': x},globals())
-
-    x = s_result
-    y = t_result
-    u_result = eval(u,{'x': x, 'y': y},globals())
-    return u_result
-
-
-print(Calc(s, t, u))
+        x = s_result
+        y = t_result
+        u_result = eval(u,{'x': x, 'y': y},globals())
+        return u_result
+    return inner
+calc_x = Calc(s, t, u)
+print(calc_x(x_val))
