acb5dd1d6f4b7019f4bf10d5bfc70646387057ae
469565267 <469565267@qq.com>
2025-11-24 16:53:35 +0300

    hw

--- 20251118/1/prog.py
+++ 20251118/1/prog.py
@@ -1,24 +1,21 @@
 import sys
 
 data = sys.stdin.buffer.read()
-#print(data)
+
+if len(data) == 0:
+    sys.exit(0)
+
 N = data[0]
 tail = data[1:]
-length = len(tail)
-output = []
-if length < N:
-    for i in range(N):
-        if i < length:
-            output.append(tail[i:i+1].strip(b'\n'))
-        else:
-            output.append(b'')
-else:
-    for i in range(N):
-        start = int(i * length / N)
-        end = int((i + 1) * length / N)
-        output.append(tail[start:end].strip(b'\n'))
-output.sort()
-#print(output)
-out_by = bytes([N]) + b''.join(output)
+L = len(tail)
 
-sys.stdout.buffer.write(out_by)
+parts = []
+for i in range(N):
+    start = int(i * L / N)
+    end = int((i + 1) * L / N)
+    part = tail[start:end]
+    parts.append(part)
+
+parts.sort()
+result = bytes([N]) + b''.join(parts)
+sys.stdout.buffer.write(result)
